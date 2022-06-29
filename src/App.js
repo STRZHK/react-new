c47aea725dad017b82ecd61b2c4e9f93195d4cad
STRZHK <oleksa.stryzhak@gmail.com>
2022-06-29 23:55:34 +0300

    HW4

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+//TASK 1
 
-function App() {
+// import {useForm} from "react-hook-form";
+//
+// export default function App() {
+//   let {register, handleSubmit} = useForm();
+//   let submit = (item) => {
+//     fetch('https://jsonplaceholder.typicode.com/posts', {
+//       method: 'POST',
+//       body: JSON.stringify(item),
+//       headers: {'Content-type': 'application/json; charset=UTF-8'},
+//     })
+//         .then((response) => response.json())
+//         .then((json) => console.log(json));  }
+//   return (
+//     <div>
+//       <form onSubmit={handleSubmit(submit)}>
+//         <input type='text' {...register("Name", {required:true})}/>
+//         <input type='text' {...register("Surname", {required:true})}/>
+//         <input type='age' {...register("Age")}/>
+//         <button>Save</button>
+//       </form>
+//     </div>
+//   );
+// }
+
+//TASK 2 > COMMENTS
+
+import {useForm} from "react-hook-form";
+
+export default function App() {
+  let {register, handleSubmit} = useForm();
+  let submit = (item) => {
+    fetch('http://jsonplaceholder.typicode.com/comments', {
+      method: 'POST',
+      body: JSON.stringify(item),
+      headers: {'Content-type': 'application/json; charset=UTF-8'},
+    })
+        .then((response) => response.json())
+        .then((json) => console.log(json));  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <form onSubmit={handleSubmit(submit)}>
+          <input type='text' {...register("Comment", {required:true})}/>
+          <button>Save</button>
+        </form>
+      </div>
   );
 }
-
-export default App;
